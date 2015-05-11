61aee71c57fc2f32e53b5383b501eac38456e705
Florian Bacher <bacherfl@gmail.com>
2015-05-11 16:16:24 +0200

    removed unused imports

--- src/main/java/server/ServerApplication.java
+++ src/main/java/server/ServerApplication.java
@@ -1,17 +1,8 @@
 package server;
 
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import server.service.Downloader;
 
 @SpringBootApplication
 public class ServerApplication {
